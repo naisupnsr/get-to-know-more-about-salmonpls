b9edee4c0d5216799e1fb0a54692fc4b8a3916d3
salmon-pls <soz.pphanus@gmail.com>
2026-04-05 16:11:14 +0700

    update responsive

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import "./App.css";
 function App() {
   const [position, setPosition] = useState(null);
   const [isResizing, setIsResizing] = useState(false); // Track resize state
+  const [buttonWidth, setButtonWidth] = useState(null);
   const containerRef = useRef(null);
   const anchorRef = useRef(null);
   const timer = useRef(null);
@@ -17,6 +18,9 @@ function App() {
     if (anchorRef.current && containerRef.current) {
       const containerRect = containerRef.current.getBoundingClientRect();
       const anchorRect = anchorRef.current.getBoundingClientRect();
+
+       setButtonWidth(anchorRect.width); 
+
       setPosition({
         x: anchorRect.left - containerRect.left + anchorRect.width / 2,
         y: anchorRect.top - containerRect.top + anchorRect.height / 2,
@@ -57,56 +61,59 @@ function App() {
   };
 
   const btnStyle =
-    "text-[#1900FF] back-white w-100 hover:bg-[#1900FF] font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded text-center block";
+    "text-[#1900FF] back-white w-full hover:bg-[#1900FF] font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded text-center block";
 
   return (
-      <section
-        ref={containerRef}
-        id="center"
-        onMouseLeave={updateHome} // <--- พอมือออก ให้วิ่งกลับไปหาจุด Anchor
-        className="relative"
-        style={{ display: "grid", gridTemplateColumns: "repeat(auto-fit, minmax(300px, 1fr))", gap: "16px" }}
-      >
-        <div className="p-5">
-          <h1 style={{ fontSize: '2rem', color: '#1900FF'}} className="!m-5">salmonpls!</h1>
-          <p >5.9 kg frozen salmon on the shelf</p>
-          <p>hobby artist?— fulltime programmer</p>
-          <img width="300" height="200" src={fishWithHeart} alt="logo?" />
-        </div>
+    <section
+      ref={containerRef}
+      id="center"
+      onMouseLeave={updateHome} // <--- พอมือออก ให้วิ่งกลับไปหาจุด Anchor
+      className="relative"
+      style={{ display: "grid", gridTemplateColumns: "repeat(auto-fit, minmax(300px, 1fr))", gap: "16px" }}
+    >
+      <div className="p-5">
+        <h1 style={{ fontSize: "2rem", color: "#1900FF" }} className="!m-5">
+          salmonpls!
+        </h1>
+        <p>5.9 kg frozen salmon on the shelf</p>
+        <p>hobby artist?— fulltime programmer</p>
+        <img width="300" height="200" src={fishWithHeart} alt="logo?" />
+      </div>
 
-        <div className="flex flex-col items-center gap-4 p-5">
-          <a href="https://www.facebook.com/schnenschine" className={btnStyle}>
-            Facebook
-          </a>
-          <a href="https://x.com/salmononweekend" className={btnStyle}>
-            Twitter/X
-          </a>
-          <a href="https://www.instagram.com/schnenschine" className={btnStyle}>
-            Instagram
-          </a>
+      <div className="flex flex-col items-center gap-4 p-5 w-[calc(100%-50px)] max-w-[600px] mx-auto">
+        <a href="https://www.facebook.com/schnenschine" className={btnStyle}>
+          Facebook
+        </a>
+        <a href="https://x.com/salmononweekend" className={btnStyle}>
+          Twitter/X
+        </a>
+        <a href="https://www.instagram.com/schnenschine" className={btnStyle}>
+          Instagram
+        </a>
 
-          <div ref={anchorRef} className="h-[100px] w-[100px] bg-transparent"></div>
-        </div>
+        <div ref={anchorRef} className="h-[100px] w-full invisible bg-transparent"></div>
 
         {position && (
           <a
-          href="https://youtu.be/dQw4w9WgXcQ?si=bfrWWqPHjLYWIjEs"
-          // onClick={}
+            href="https://youtu.be/dQw4w9WgXcQ?si=bfrWWqPHjLYWIjEs"
+            // onClick={}
             onMouseEnter={moveButton}
             style={{
               position: "absolute",
               left: `${position?.x}px`,
               top: `${position?.y}px`,
+              width: `${buttonWidth}px`, 
               transform: "translate(-50%, -50%)",
               transition: isResizing ? "none" : "all 0.4s cubic-bezier(0.34, 1.56, 0.64, 1)",
               zIndex: 50,
             }}
             className={btnStyle}
           >
-          Commission Info          
+            Commission Info
           </a>
         )}
-      </section>
+      </div>
+    </section>
   );
 }
 
